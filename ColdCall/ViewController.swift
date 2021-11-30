5b77c845aec67ef9538cb5063a6ff4841d99a929
Linah-alkhurayyif <73877590+Linah-alkhurayyif@users.noreply.github.com>
2021-12-01 01:23:36 +0300

    Linah

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -11,27 +11,18 @@ class ViewController: UIViewController {
  
     let nameList = ["Liam","Olivia","Noah","Emma" , "Oliver" ,"Ava", "Elijah","Charlotte","William", "Sophia","James","Amelia","Benjamin","Isabella"
 ,"Lucas","Mia","Henry","Evelyn","Alexander","Harper"]
-    var currentName = 0
+    var currentName:Int = 0
     @IBOutlet weak var nameLabel: UILabel!
     
     @IBAction func Cold_Call(_ sender: UIButton) {
-        UIupdate()
-        if currentName < nameList.count - 1{
-            currentName = currentName + 1
-        }else{
-            currentName = 0
-            
-        }
+        currentName = Int.random(in: 0..<20)
+        nameLabel.text = nameList[currentName]
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         nameLabel.text = "Ready?"
-    }
-
-    func UIupdate() {
-        nameLabel.text = nameList[currentName]
     }
 }
 
